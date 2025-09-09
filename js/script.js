0f5b48d0c93fbde0250779168cf52d5bf9ce2b71
Johann-Fera <johannarruda.dev2024@gmail.com>
2025-09-09 11:57:48 -0300

    sarva

--- js/script.js
+++ js/script.js
@@ -18,5 +18,32 @@ document.addEventListener('DOMContentLoaded', () =>{
 
         })
     })
-    
+    const itemcart = document.getElementById('card-items-container');
+    const cartvalue = document.getElementById('card-total-value');
+    const checkout = document.getElementById('checkout-btn');
+
+    if (itemcart) {
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        let total = 0;
+        if (cart.length == 0) {
+            console.log("tienes nada")
+        } else {
+            console.log("tienes algo")
+            itemcart.innerHTML = "";
+            cart.forEach(produto =>{
+                const cartitem = document.createElement('div');
+                console.log("isso funfa")
+                cartitem.classList.add('cart-item');
+                cartitem.innerHTML = `
+                <span>${produto.nome}</span>
+                <span>R$ ${produto.preco.toFixed(2)}</span>
+                `
+                itemcart.appendChild(cartitem)
+                total += produto.price;
+            }) 
+        }
+        cartvalue.textContent = `R$ ${total.toFixed(2)}`;
+    }
+    const 
+
 })
