f20f42d8f500baa3ff40b4bae5b8a08b6a387b30
Johann-Fera <johannarruda.dev2024@gmail.com>
2025-09-11 09:23:58 -0300

    imagem

--- backend/djambra/consumiveis/models.py
+++ backend/djambra/consumiveis/models.py
@@ -8,5 +8,6 @@ class Consumivel(models.Model):
     nome = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, related_name='consumives', on_delete=models.CASCADE)
+    imagem = models.ImageField(upload_to='consumiveis/', blank=True, null=True)
     def __str__(self):
         return self.nome
